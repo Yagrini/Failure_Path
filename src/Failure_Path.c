7127068cb380d4ad76806c0106d617bad9baf6b5
Youness Lagrini <lagyouness@gmail.com>
2019-01-21 19:17:36 +0100

    Failure_path

---
+++ src/Failure_Path.c
@@ -0,0 +1,80 @@
+#include "Failure_Path.h" 
+
+
+int Failure_Path(TBOOL X1,TBOOL X2,TBOOL X3,TBOOL X4,TBOOL X5,TBOOL X6, TBOOL X7, TBOOL X8)
+{
+	int retval = -1;
+if (X1) 
+{
+	retval = 9;
+	
+} else 
+{
+	
+	if (X2 && X8)
+	{
+		retval = 3;
+	}else
+	{
+		if (X2 && !(X8) )
+			{
+				retval = 11;
+			}	
+			else
+			{
+				if (X3 && X8)
+				{
+					retval = 8;
+				}
+				else
+				{
+						if (X3 && !X8)
+						{
+							retval =7;
+						}
+						else
+						{
+							if (X4 && X5)
+							{
+									retval=2;
+							}
+							else
+							{
+								if ( X6 && X7 && X8)
+								{
+									retval = 5;
+								}
+								else
+								{
+									if ( X6 && !X7)
+									{
+										retval = 6;
+									}
+									else
+									{
+										if ( X6 && X7 && !X8)
+										{
+												retval = 8;
+										}
+										else
+										{
+											retval = 0;
+										}
+									}
+								}
+							}
+						}
+				}
+			}		
+	}
+	}
+return retval;
+}
+
+//int main ()
+//{
+//	TBOOL X1_param , X2_param, X3_param,X4_param,X5_param ,X6_param,X7_param,X8_param;
+//	int ret;
+//	ret=Failure_Path(X1_param , X2_param, X3_param,X4_param,X5_param ,X6_param,X7_param,X8_param);
+	
+//}
---
+++ src/Failure_Path.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+typedef  enum {TRUE = 1, FALSE = 0} TBOOL;
+
+extern int Failure_Path(TBOOL X1,TBOOL X2,TBOOL X3,TBOOL X4,TBOOL X5,TBOOL X6, TBOOL X7, TBOOL X8);
+
